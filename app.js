31953420288b0e0ff0f2cc04e0f647ea97455aa0
Mazharrais <mazharmajo72@gmail.com>
2023-11-01 20:31:55 +0500

    add file

--- app.js
+++ app.js
@@ -5,51 +5,87 @@
 var hour = 0;
 var minute = 0;
 var sec = 0;
-var milisec = 0;
-var interval;
+var count = 0;
 
-var hourHeading = document.getElementById("hour");
-var minuteHeading = document.getElementById("minute");
-var secHeading= document.getElementById("sec");
-var milisecHeading = document.getElementById("milisec");
 
-function startTimer() {
-    milisec++;
-    milisecHeading.innerHTML = milisec;
-    if(milisec >= 100) {
-        sec++;
-        secHeading.innerHTML = sec;
-        milisec = 0;
-    }
-    else if(sec >= 60) {
-        minute++;
-        minuteHeading.innerHTML = minute;
-        sec = 0;
-    }
-    else if(minute >= 60) {
-        hour++;
-        hourHeading.innerHTML = hour;
-        minute = 0;
-    }
-}
+var timer = flase;
+
+
 
 function start(){
-    interval = setInterval(startTimer, 10);
+    timer = true;
+    stopWatch()
 
 }
 
-function pause() {
-    clearInterval(interval);
+function stop() {
+    timer = false;
 }
 
-function reset() {
-     hour = 0;
+function reSet() {
+    timer = false;
+
+
+   hour = 0;
      minute = 0;
      sec = 0;
-     milisec = 0;
-     hourHeading.innerHTML = hour;
-     minuteHeading.innerHTML = minute;
-     secHeading.innerHTML = sec;
-     milisecHeading.innerHTML = milisec;
-     pause();
+     count = 0;
+
+    
+   document.getElementById("hr").innerHTML = "00";
+   document.getElementById("mint").innerHTML = "00";
+    document.getElementById("sec").innerHTML ="00";
+    document.getElementById("count").innerHTML = "00";
+    
+    
+}
+
+function stopWatch(){
+  if(timer == true){
+    count = count+1;
+
+    if(count == 60){
+        sec = sec+1;
+        count = 0;
+    }
+    if(sec == 60){
+        minute = minute+1;
+        sec = 0
+    }
+   if(mint == 60){
+    hour = hour+1;
+    minute = 0;
+    sec = 0;
+     
+
+   var hourString = hour;
+   var minuteString = minute;
+   var secString = sec;
+   var countString = count;
+
+   if(hour < 10){
+      hourString = "0" + hourString;
+   }
+   if(minute < 10){
+    minuteString = "0" + minuteString;
+   }
+   if(secString < 10){
+    secString = "0" + secString;
+   }
+  if(countString < 10){
+    countString = "0" + countString;
+  }
+
+
+
+
+   }
+   document.getElementById("hr").innerHTML = hour;
+   document.getElementById("mint").innerHTML = minute;
+    document.getElementById("sec").innerHTML = sec;
+    document.getElementById("count").innerHTML = count;
+
+
+    setTimeout(stopWatch , 10);
+  }
 }
